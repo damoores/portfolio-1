6c1635dd7842ae5f6a043cd30c16557e24bdc9fd
rashidbaset <rashid.baset@gmail.com>
2016-05-10 14:43:40 -0700

    added SMACSS files, portfolio data and portfolio object constructor.

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+portfolio = [];
+
+function Portfolio (opts) {
+  this.title = opts.title;
+  this.publishedOn = opts.publishedOn;
+  this.tagline = opts.tagline;
+  this.description = opts.description;
+  this.tag = opts.tag;
+}
---
+++ scripts/portfoliodata.js
@@ -0,0 +1,30 @@
+portfolioData = [
+  {
+    title: 'Hacker Hangout',
+    published: 'test hh date',
+    tagline: 'test hh tagline',
+    description: 'test hh description',
+    tag: '#test hh tag',
+  },
+  {
+    title: 'Bus Mall',
+    published: 'test bm date',
+    tagline: 'test bm tagline',
+    description: 'test bb description',
+    tag: '#test bb tag',
+  },
+  {
+    title: 'Pizza Odyssey',
+    published: 'test po date',
+    tagline: 'test po tagline',
+    description: 'test po description',
+    tag: '#test po tag',
+  },
+  {
+    title: 'About Me',
+    published: 'test am date',
+    tagline: 'test am tagline',
+    description: 'test am description',
+    tag: 'test am tag',
+  }
+];
