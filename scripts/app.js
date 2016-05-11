7efb232d8d54c28bcaf618764e4855b10028ee35
rashidbaset <rashid.baset@gmail.com>
2016-05-11 09:15:54 -0700

    updated files

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-portfolio = [];
+portfolioArray = [];
 
 function Portfolio (opts) {
   this.title = opts.title;
@@ -7,3 +7,25 @@ function Portfolio (opts) {
   this.description = opts.description;
   this.tag = opts.tag;
 }
+
+Portfolio.prototype.toHtml = function() {
+  var $newPortfolio = $('article.template').clone();
+
+  $newPortfolio.find('header h1').text(this.title);
+  $newPortfolio.find('.project-description').html(this.description);
+  $newPortfolio.find('.published').html(this.publishedOn);
+  $newPortfolio.find('.tagline').html(this.tagline);
+  $newPortfolio.find('.published').html(this.publishedOn);
+  $newPortfolio.find('.tag').html(this.tag);
+
+  $newPortfolio.removeClass('template');
+  return $newPortfolio;
+};
+
+portfolioData.forEach(function(ele){
+  portfolioArray.push(new Portfolio(ele));
+});
+
+portfolioArray.forEach(function(p){
+  $('#projects').append(p.toHtml());
+});
